230d63c1a869c4ce3771da4067b339e3a28eb30f
BearYep <92510812+BearYep@users.noreply.github.com>
2024-05-18 04:19:12 +0800

    Add random test mode

--- Encryption/__init__.py
+++ Encryption/__init__.py
@@ -6,13 +6,16 @@ import Encryption.braille_cipher as braille
 from Encryption.FibraEncryption_final import FibraEncryption
 from Encryption.one_connect import one_con
 
+import random
+
 rrr = RRR()
 Fibra = FibraEncryption()
 oneCon = one_con()
 
 class CipherMachine:
     def __init__(self):
-        pass
+        self.random_times = 100
+        self.information_flag = False
     
     def encode(self, plain_text = None, key = None):
         cipher_text = webC.fold_and_encrypt(plain_text, key)
@@ -33,3 +36,33 @@ class CipherMachine:
         plain_text = webC.decrypt_and_unfold(plain_text, key)
 
         return plain_text
+    
+    def random_test(self):
+        for _ in range(self.random_times):
+            length = random.randint(1,5)
+            key = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k = length))
+
+            length = 5
+            plaintext = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k = length))
+            # Encrypt
+            cipher_text = self.encode(plaintext, key)
+
+            # Decrypt
+            decrypted_text = self.decode(cipher_text, key)
+
+            # Check if decryption result matches plaintext
+            if decrypted_text != plaintext:
+                print("Decryption error!")
+                print("Key:", key)
+                print("Plaintext:", plaintext)
+                print("Cipher text:", cipher_text)
+                print("Decrypted_text:", decrypted_text)
+                print("------------------------")
+            else:
+                print("Decryption pass!")
+                if self.information_flag:
+                    print("Key:", key)
+                    print("Plaintext:", plaintext)
+                    print("Cipher text:", cipher_text)
+                    print("Decrypted_text:", decrypted_text)
+                    print("------------------------")
